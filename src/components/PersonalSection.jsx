00b5b3c7c4a04760dda035fbf971a8c3183fe498
CopyypoC <william.wang650@gmail.com>
2025-05-10 20:26:00 -0700

    feat: add App colors

--- src/components/PersonalSection.jsx
+++ src/components/PersonalSection.jsx
@@ -2,9 +2,11 @@ export function PersonalSection({ personalItem }) {
   return (
     <header>
       <h2>{personalItem.name}</h2>
-      <p>{personalItem.email}</p>
-      <p>{personalItem.phone}</p>
-      <p>{personalItem.location}</p>
+      <div className="personal-bottom">
+        <p>{personalItem.email}</p>
+        <p>{personalItem.phone}</p>
+        <p>{personalItem.location}</p>
+      </div>
     </header>
   );
 }
